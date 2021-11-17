f7d0c7f3dc873c2def774f47e928ca9dac01f1ce
hannahlegros <hannahrlegros@gmail.com>
2021-11-17 10:05:21 -0500

    got another route working

--- controllers/byName.js
+++ controllers/byName.js
@@ -24,45 +24,44 @@ router.get('/:id', (req, res) => {
     // console.log('this is req.params.id: ', req.params.id)
     axios.get(`http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
         .then(apiResults => {
-            // console.log('this is apiRes.data.drinks: ', apiRes.data.drinks)
-            let name = apiRes.data.drinks.strDrink
-            console.log('this is apiResults:', apiResults)
-            // let ingOne = apiRes.data.drinks.strIngredient1
-            // let ingTwo = apiRes.data.drinks.strIngredient2
-            // let ingThree = apiRes.data.drinks.strIngredient3
-            // let ingFour = apiRes.data.drinks.strIngredient4
-            // let ingFive = apiRes.data.drinks.strIngredient5
-            // let ingSix= apiRes.data.drinks.strIngredient6
-            // let ingSeven = apiRes.data.drinks.strIngredient7
-            // let ingEight = apiRes.data.drinks.strIngredient8
-            // let ingNine = apiRes.data.drinks.strIngredient9
-            // let ingTen = apiRes.data.drinks.strIngredient10
-            // let ingEleven = apiRes.data.drinks.strIngredient11
-            // let ingTwelve = apiRes.data.drinks.strIngredient12
-            // let ingThirteen = apiRes.data.drinks.strIngredient13
-            // let ingFourteen = apiRes.data.drinks.strIngredient14
-            // let ingFifteen = apiRes.data.drinks.strIngredient15
-            // let measureOne = apiRes.data.drinks.strMeasure1
-            // let measureTwo = apiRes.data.drinks.strMeasure2
-            // let measureThree = apiRes.data.drinks.strMeasure3
-            // let measureFour = apiRes.data.drinks.strMeasure4
-            // let measureFive= apiRes.data.drinks.strMeasure5
-            // let measureSix= apiRes.data.drinks.strMeasure6
-            // let measureSeven = apiRes.data.drinks.strMeasure7
-            // let measureEight = apiRes.data.drinks.strMeasure8
-            // let measureNine = apiRes.data.drinks.strMeasure9
-            // let measureTen = apiRes.data.drinks.strMeasure10
-            // let measureEleven = apiRes.data.drinks.strMeasure11
-            // let measureTwelve = apiRes.data.drinks.strMeasure12
-            // let measureThirteen = apiRes.data.drinks.strMeasure13
-            // let measureFourteen = apiRes.data.drinks.strMeasure14
-            // let measureFifteen = apiRes.data.drinks.strMeasure15
-            // let instructions = apiRes.data.drinks.strInstructions
-            res.render('drinkDetail', {name:name})
-                // , ingOne:ingOne, ingTwo:ingTwo, ingThree:ingThree, ingFour:ingFour, ingFive:ingFive, ingSix:ingSix, ingSeven:ingSeven, ingEight:ingEight, ingNine:ingNine, ingTen:ingTen, ingEleven:ingEleven, ingTwelve:ingTwelve, ingThirteen:ingThirteen, ingFourteen:ingFourteen, ingFifteen:ingFifteen, measureOne:measureOne, measureTwo:measureTwo, measureThree:measureThree, measureFour:measureFour, measureFive:measureFive, measureSix:measureSix, measureSeven:measureSeven, measureEight:measureEight, measureNine:measureNine, measureTen:measureTen, measureEleven:measureEleven, measureTwelve:measureTwelve, measureThirteen:measureThirteen, measureFourteen:measureFourteen, measureFifteen:measureFifteen, instructions:instructions})
+            console.log('this is apiRes.data.drinks: ', apiResults.data.drinks)
+            console.log('this is apiRes.data.strIngredient1: ', apiResults.data.drinks[0].strIngredient1)
+            let name = apiResults.data.drinks[0].strDrink
+            let ingOne = apiResults.data.drinks[0].strIngredient1
+            let ingTwo = apiResults.data.drinks[0].strIngredient2
+            let ingThree = apiResults.data.drinks[0].strIngredient3
+            let ingFour = apiResults.data.drinks[0].strIngredient4
+            let ingFive = apiResults.data.drinks[0].strIngredient5
+            let ingSix= apiResults.data.drinks[0].strIngredient6
+            let ingSeven = apiResults.data.drinks[0].strIngredient7
+            let ingEight = apiResults.data.drinks[0].strIngredient8
+            let ingNine = apiResults.data.drinks[0].strIngredient9
+            let ingTen = apiResults.data.drinks[0].strIngredient10
+            let ingEleven = apiResults.data.drinks[0].strIngredient11
+            let ingTwelve = apiResults.data.drinks[0].strIngredient12
+            let ingThirteen = apiResults.data.drinks[0].strIngredient13
+            let ingFourteen = apiResults.data.drinks[0].strIngredient14
+            let ingFifteen = apiResults.data.drinks[0].strIngredient15
+            let measureOne = apiResults.data.drinks[0].strMeasure1
+            let measureTwo = apiResults.data.drinks[0].strMeasure2
+            let measureThree = apiResults.data.drinks[0].strMeasure3
+            let measureFour = apiResults.data.drinks[0].strMeasure4
+            let measureFive= apiResults.data.drinks[0].strMeasure5
+            let measureSix= apiResults.data.drinks[0].strMeasure6
+            let measureSeven = apiResults.data.drinks[0].strMeasure7
+            let measureEight = apiResults.data.drinks[0].strMeasure8
+            let measureNine = apiResults.data.drinks[0].strMeasure9
+            let measureTen = apiResults.data.drinks[0].strMeasure10
+            let measureEleven = apiResults.data.drinks[0].strMeasure11
+            let measureTwelve = apiResults.data.drinks[0].strMeasure12
+            let measureThirteen = apiResults.data.drinks[0].strMeasure13
+            let measureFourteen = apiResults.data.drinks[0].strMeasure14
+            let measureFifteen = apiResults.data.drinks[0].strMeasure15
+            let instructions = apiResults.data.drinks[0].strInstructions
+            res.render('drinkDetail.ejs', {name: name, ingOne: ingOne, ingTwo: ingTwo, ingThree: ingThree, ingFour: ingFour, ingFive: ingFive, ingSix: ingSix, ingSeven: ingSeven, ingEight: ingEight, ingNine: ingNine, ingTen: ingTen, ingEleven: ingEleven, ingTwelve: ingTwelve, ingThirteen: ingThirteen, ingFourteen: ingFourteen, ingFifteen: ingFifteen, measureOne: measureOne, measureTwo: measureTwo, measureThree: measureThree, measureFour: measureFour, measureFive: measureFive, measureSix: measureSix, measureSeven: measureSeven, measureEight: measureEight, measureNine: measureNine, measureTen: measureTen, measureEleven: measureEleven, measureTwelve: measureTwelve, measureThirteen: measureThirteen, measureFourteen: measureFourteen, measureFifteen: measureFifteen, instructions: instructions})
         })
         .catch(err => {
-            console.log('err')
+            console.log(err)
         })
 })
 
