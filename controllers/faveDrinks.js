efb0ca02f01e5f36c67539ebb5eeed47d90b63e2
hannahlegros <hannahrlegros@gmail.com>
2021-11-17 18:44:40 -0500

    add another dependency, got faves showing

--- controllers/faveDrinks.js
+++ controllers/faveDrinks.js
@@ -1,11 +1,13 @@
 let express = require('express')
 let db = require('../models')
 let router = express.Router()
+const byName = require('./byName')
 
-// GET show drinks add to Love It
+// GET show drinks added to Love It
 router.get('/', (req, res) => {
     db.recipe.findAll()
         .then(faves => {
+            console.log(faves)
             res.render('faveDrinks.ejs', {loveIt: faves})
         })
         .catch(error => {
@@ -14,13 +16,20 @@ router.get('/', (req, res) => {
 })
 
 // POST add favorite drink to Love It
-router.post('/add', (req, res) => {
+router.post('/addLoveIt', (req, res) => {
     const data = JSON.parse(JSON.stringify(req.body))
     console.log('this is data:', data)
-    // db.recipe.create({
-
-
-    // })
+    db.recipe.create({
+        name: data.drinkName[0],
+        drinkId: data.drinkName[1]
+    })
+    .then (createdFave => {
+        // console.log('db instance created: ', createdFave)
+        res.redirect('/myLoveIt')
+    })
+    .catch(error => {
+        console.log(error)
+    })
 })
 
 
