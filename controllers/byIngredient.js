44018dc0245e23449588cbf207c084440ab932d0
hannahlegros <hannahrlegros@gmail.com>
2021-11-18 16:04:19 -0500

    ingredient displaying when searched

--- controllers/byIngredient.js
+++ controllers/byIngredient.js
@@ -6,13 +6,13 @@ let axios = require('axios')
 // POST  search for ingredient
 router.post('/', (req, res) => {
     let drinkIngr = req.body.byIngr
-    // console.log('drink ingr:', drinkIngr)
-    axios.get(`www.thecocktaildb.com/api/json/v1/1/search.php?i=${drinkIngr}`)
+    console.log('drink ingr:', drinkIngr)
+    axios.get(`http://www.thecocktaildb.com/api/json/v1/1/search.php?i=${drinkIngr}`)
     .then(apiRes => {
         // console.log('this is apiRes:', apiRes)
         // console.log('this is apiRes.data:', apiRes.data)
         // console.log('this is apiRes.data.drinks:', apiRes.data.drinks)
-        const ingrRes = apiRes.data.ingredients[0]
+        const ingrRes = apiRes.data.ingredients
         res.render('ingrResults', {ingrRes: ingrRes})
     })
     .catch(error =>{
@@ -20,7 +20,7 @@ router.post('/', (req, res) => {
     })
 })
 
-// GET route to search for ingredients(maybe needs to be it's own controller?)
+
 // router.get('/', (req, res) => {
 
 // })
